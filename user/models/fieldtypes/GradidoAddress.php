bb5fc6aeb93018f129cbb160737e061f5cc506b6
einhornimmond <info@einhornimmond.de>
2024-03-05 11:23:10 +0100

    add about:blank for gradido link

--- user/models/fieldtypes/GradidoAddress.php
+++ user/models/fieldtypes/GradidoAddress.php
@@ -3,7 +3,8 @@
 namespace humhub\modules\gradidohumhubmodule\user\models\fieldtypes;
 
 use humhub\modules\user\models\fieldtype\Text;
-use Yii;
+use humhub\modules\user\models\User;
+use yii\helpers\Html;
 
 /**
  * ProfileFieldTypeGradidoAddress handles gradido address profile field.
@@ -27,4 +28,18 @@ class GradidoAddress extends Text
         }
         return $textFieldRules;
     }   
+
+    public function getUserValue(User $user, $raw = true): string
+    {
+        $internalName = $this->profileField->internal_name;
+        $value = $user->profile->$internalName;
+
+        if (!$raw && (in_array($this->validator, [self::VALIDATOR_EMAIL, self::VALIDATOR_URL]) || !empty($this->linkPrefix))) {
+            $linkPrefix = ($this->validator === self::VALIDATOR_EMAIL) ? 'mailto:' : $this->linkPrefix;
+            return Html::a(Html::encode($value), $linkPrefix . $value, ['target' => 'about:_blank']);
+        }
+
+        return Html::encode($value);
+    }
+
 };
